0079dab6f73cc2dbfb8abc218a12208a0a7fa2c8
Adam M. Gorzelak <134437909+amg-k@users.noreply.github.com>
2023-05-28 20:31:05 +0200

    Change area_calc function
    Now circle_area_calc(radius) function exist

--- gitTest.py
+++ gitTest.py
@@ -7,13 +7,13 @@ def num_sum(num1, num2):
 def num_multiple(fix1, fix2):
     return fix1 * fix2
 
-def area_calc(side1, side2):
-    return side1 * side2
+def circle_area_calc(radius):
+    return 3.14 * radius ** 2
 
 def return_random(start, end):
     return randint(start, end)
 
 print(num_sum(28, 18))
 print(num_multiple(14, 18))
-print("Area of rectangle is: ", area_calc(8, 4))
+print("Area of circle is: ", circle_area_calc(8))
 print(return_random(1, 11))
