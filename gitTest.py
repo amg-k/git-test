5670233270ed1f97ffb4cece6af41fe90505f34e
amg-k <adamgorz@gmail.com>
2023-05-28 19:43:20 +0200

    Added area calculation function

--- gitTest.py
+++ gitTest.py
@@ -5,6 +5,9 @@ def num_sum(num1, num2):
 def num_multiple(num1, num2):
     return num1 * num2
 
+def area_calc(side1, side2):
+    return side1 * side2
 
 print(num_sum(28, 18))
 print(num_multiple(14, 18))
+print("Area of rectangle is: ", area_calc(8, 4))
