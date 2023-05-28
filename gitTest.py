3c43ae4e19b614ca1f6f0ee1cace858976582d13
amg-k <adamgorz@gmail.com>
2023-05-28 20:05:49 +0200

    Added random function

--- gitTest.py
+++ gitTest.py
@@ -1,3 +1,5 @@
+from random import randint
+
 def num_sum(num1, num2):
     return num1 + num2
 
@@ -8,6 +10,10 @@ def num_multiple(num1, num2):
 def area_calc(side1, side2):
     return side1 * side2
 
+def return_random(start, end):
+    return randint(start, end)
+
 print(num_sum(28, 18))
 print(num_multiple(14, 18))
 print("Area of rectangle is: ", area_calc(8, 4))
+print(return_random(1, 11))
